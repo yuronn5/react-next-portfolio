410278f6fa35f0d83ab4ffc7ba4f810b50411095
yurii bilous <yurii.b@elogic.com.ua>
2024-11-10 19:43:56 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,10 @@ export const Navbar = () => {
         <ul className="flex flex-row p-2 space-x-8">
           {navLinks.map((link, index) => (
             <li key={index}>
-              <Link href={link.path} className="transform hover:text-white/50 transition-all duration-300 ease-in-out">
+              <Link
+                href={link.path}
+                className="transform hover:text-white/50 transition-all duration-300 ease-in-out"
+              >
                 {link.title}
               </Link>
             </li>
@@ -36,20 +39,27 @@ export const Navbar = () => {
         </ul>
       </div>
 
-      <div onClick={toggleNav} className="md:hidden absolute top-5 right-14 border rounded z-50 text-white/70 border-white/70 p-2">
+      <div
+        onClick={toggleNav}
+        className="md:hidden absolute top-5 right-14 border rounded z-50 text-white/70 border-white/70 p-2"
+      >
         {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
       </div>
 
-      <div className={`fixed left-0 top-0 w-full h-full bg-black/90 transform transition-transform duration-300 ${nav ? 'translate-x-0' : 'translate-x-full'}`}>
-          <ul className="flex flex-col items-center justify-center space-y-8 h-full">
-            {navLinks.map((link, index) => (
-                <li key={index}>
-                <Link href={link.path} onClick={closeNav} className="text-5xl">
-                    {link.title}
-                </Link>
-                </li>
-            ))}
-          </ul>
+      <div
+        className={`fixed left-0 top-0 w-full h-full bg-black/90 transform transition-transform duration-300 ${
+          nav ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <ul className="flex flex-col items-center justify-center space-y-8 h-full">
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <Link href={link.path} onClick={closeNav} className="text-5xl">
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
