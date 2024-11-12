972177d665afe70498fad62cbf4eee20de41a710
yurii bilous <yurii.b@elogic.com.ua>
2024-11-12 22:20:42 +0100

    stack section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import { Stack } from "@/components/Stack";
 
 export default function Home() {
   return (
   <>
     <Hero />
+    <Stack />
   </>
   );
 }
---
+++ components/Stack.tsx
@@ -0,0 +1,35 @@
+import { color } from "framer-motion";
+import React from "react";
+import { FaReact } from "react-icons/fa";
+import { IoLogoNodejs } from "react-icons/io";
+import { SiTypescript } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
+const stackItems = [
+    {id: 1, name: "React", icon: FaReact, color: "#61DAFB"},
+    {id: 2, name: "Node.js", icon: IoLogoNodejs, color: "#339933"},
+    {id: 3, name: "TypeScript", icon: SiTypescript, color: "#3178A6"},
+    {id: 4, name: "MongoDB", icon: SiMongodb, color: "#47A248"},
+    {id: 5, name: "Next.js", icon: TbBrandNextjs, color: "#000000"},
+]
+
+export const Stack = () => {
+    return (
+        <section className="py-16 glass">
+            <div className="max-w-[1200px] mx-auto px-4 text-center">
+                <h2 className="text-5xl font-bold mb-4 text-gray-200">My Stack</h2>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
+                    {stackItems.map((item) => (
+                        <div key={item.id} className="flex flex-col items-center justify-center rounded-xl p-4">
+                            <div className="mb-4 bg-white/10 p-6 rounded-xl">
+                                {React.createElement(item.icon, {className: "w-32 h-32", style: {color: item.color}})}
+                            </div>
+                            <p className="text-gray-400 font-semibold">{item.name}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
