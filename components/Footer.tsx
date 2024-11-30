45b10d458a91caf7af1aa12b084f05eb76ab6a5d
yurii bilous <yurii.b@elogic.com.ua>
2024-11-30 11:13:51 -0600

    update footer llinks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
-import { AiFillGithub, AiOutlineTwitter, AiFillFacebook, AiFillInstagram } from 'react-icons/ai';
+import { AiFillGithub, AiOutlineTwitter, AiFillFacebook, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { FaTelegramPlane } from 'react-icons/fa';
 
 const socialLinks = [
-  { href: "https://github.com", Icon: AiFillGithub, label: "GitHub" },
-  { href: "https://x.com", Icon: AiOutlineTwitter, label: "X (Twitter)" },
-  { href: "https://facebook.com", Icon: AiFillFacebook, label: "Facebook" },
-  { href: "https://instagram.com", Icon: AiFillInstagram, label: "Instagram" },
-  { href: "https://telegram.org", Icon: FaTelegramPlane, label: "Telegram" }
+  // { href: "https://github.com", Icon: AiFillGithub, label: "GitHub" },
+  { href: "https://www.linkedin.com/in/yurii-bilous-a02185208/", Icon: AiFillLinkedin, label: "LinkedIn" },
+  // { href: "https://x.com", Icon: AiOutlineTwitter, label: "X (Twitter)" },
+  { href: "https://www.facebook.com/profile.php?id=61567791037851", Icon: AiFillFacebook, label: "Facebook" },
+  { href: "https://www.instagram.com/yourockzz/", Icon: AiFillInstagram, label: "Instagram" },
+  // { href: "https://telegram.org", Icon: FaTelegramPlane, label: "Telegram" }
 ];
 
 export const Footer = () => {
