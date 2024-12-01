8497d239daeaf102c0f5c410490463307ad404ef
yurii bilous <yurii.b@elogic.com.ua>
2024-11-30 21:31:50 -0600

    stack upd

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -4,51 +4,74 @@ import { SiTypescript } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiPhp } from "react-icons/si";
 import { FaDocker } from "react-icons/fa";
-import { SiShopify, SiMagento } from "react-icons/si";
+import {
+  SiShopify,
+  SiMagento,
+  SiHtml5,
+  SiCss3,
+  SiWordpress,
+} from "react-icons/si";
 
 const stackItems = [
   {
-    id: 5,
+    id: 1,
     name: "Shopify",
     icon: SiShopify,
     color: "#7AB55C",
   },
   {
-    id: 6,
+    id: 2,
     name: "Magento 2",
     icon: SiMagento,
     color: "#D45D26",
   },
   {
-    id: 4,
+    id: 3,
     name: "Next.js",
     icon: TbBrandNextjs,
     color: "#000000",
   },
   {
-    id: 1,
+    id: 4,
     name: "React",
     icon: FaReact,
     color: "#61DAFB",
   },
-
   {
-    id: 2,
+    id: 5,
     name: "TypeScript",
     icon: SiTypescript,
     color: "#3178A6",
   },
   {
-    id: 3,
+    id: 6,
+    name: "HTML",
+    icon: SiHtml5,
+    color: "#E44D26",
+  },
+  {
+    id: 7,
+    name: "CSS",
+    icon: SiCss3,
+    color: "#264DE4",
+  },
+  {
+    id: 8,
     name: "PHP",
     icon: SiPhp,
     color: "#8B008B",
   },
   {
-    id: 7,
+    id: 9,
     name: "Docker",
     icon: FaDocker,
     color: "#2496ED",
+  },
+  {
+    id: 10,
+    name: "Wordpress",
+    icon: SiWordpress,
+    color: "#21759B",
   },
 ];
 
